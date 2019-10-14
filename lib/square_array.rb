038345febdd037ad1192a72b01c87d9c22c0f38b
Melike KILIC <melike180400@gmail.com>
2019-10-14 23:40:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,11 @@
 def square_array(array)
   counter = 0
   array= [2,4,8]
+  Math.sqrt(array)
 
 
   while array.length>counter do
    puts array[counter]
    counter +=1
   end
- while array.length>counter do
-   Math.sqrt(array)
  end
-end
