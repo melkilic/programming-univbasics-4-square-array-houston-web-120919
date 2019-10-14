32f2bed40c2d2a0e74722db171ee382985eb318a
Melike KILIC <melike180400@gmail.com>
2019-10-14 23:46:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,10 @@
 def square_array(array)
   counter = 0
-  array= [2,4,8]
-
-
 
   while array.length>counter do
    puts array[counter]
    counter +=1
   end
  end
+  array= [2,4,8]
+  square_array(array)
